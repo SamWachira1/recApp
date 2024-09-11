5e44d3ceb48a7407db286d29a12010ef32096ec7
Samuel <187swachira@gmail.com>
2024-09-11 17:48:37 -0400

    updates

--- server/index.js
+++ server/index.js
@@ -1,11 +1,16 @@
 import express from 'express';
-import { profiles } from './routes/profiles.js';
 import mongoose from 'mongoose';
+import { MONGO_URI } from './config/keys.js';
+import { profiles } from './routes/profiles.js';
 import cors from 'cors';
 
 
 const app = express()
 app.use(express.json())
+
+mongoose.connect(MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true})
+.then(()=> console.log('MongoDB is Successfully Connected'))
+.catch(err => console.log(err))
 
 app.get('/test', ( _ ,res) =>{
     res.send('Hello World')
