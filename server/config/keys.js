e4ef5307b1d6ffb424fc02a7a4a0693a8b96d7eb
Samuel <187swachira@gmail.com>
2024-09-11 17:35:46 -0400

    created foundation for profiles including models and routes

---
+++ server/config/keys.js
@@ -0,0 +1,1 @@
+export const MONGO_URI = 'mongodb+srv://samuelwachira:Liya1995!@recapp.k6pcq.mongodb.net/'
--- server/index.js
+++ server/index.js
@@ -1,9 +1,17 @@
 import express from 'express';
+import { profiles } from './routes/profiles.js';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
 
 const app = express()
+app.use(express.json())
+
+app.get('/test', ( _ ,res) =>{
+    res.send('Hello World')
+})
+
+app.use('/api/profiles', profiles)
 
 const port = 5000 
 const callback = () => console.log(`Server is running on port ${port}`)
